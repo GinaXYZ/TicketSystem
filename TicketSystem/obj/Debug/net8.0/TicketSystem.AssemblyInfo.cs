9ab057da649a53e1a496281a182fb86c75207867
GinaXYZ <qiiina122@hotmail.de>
2025-11-12 10:33:59 +0100

    Initial commit for TicketSystem

--- TicketSystem/obj/Debug/net8.0/TicketSystem.AssemblyInfo.cs
+++ TicketSystem/obj/Debug/net8.0/TicketSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c256beb46da264203fac48200f512f0179e549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77609fcf0f15318c24726975396a6368fdcde410")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
